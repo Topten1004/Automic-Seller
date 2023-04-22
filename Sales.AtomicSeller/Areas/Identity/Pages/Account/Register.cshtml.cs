fa3a3e51fcc3c2617263e3a92810f71618031a4a
TopTen1004 <crazycoding330@gmail.com>
2023-04-22 11:23:08 -0400

    fixed bug

--- Sales.AtomicSeller/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Sales.AtomicSeller/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -31,7 +31,7 @@ namespace Sales.AtomicSeller.Areas.Identity.Pages.Account
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IServiceProvider _serviceProvider;
-        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly RoleManager<ApplicationUser> _roleManager;
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
@@ -113,90 +113,90 @@ namespace Sales.AtomicSeller.Areas.Identity.Pages.Account
         }
 
 
-        //public async Task OnGetAsync(string returnUrl = null)
-        //{
-        //    ReturnUrl = returnUrl;
-        //    ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        //}
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
 
-        //public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        //{
-        //    returnUrl ??= Url.Content("~/");
-        //    ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = CreateUser();
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = CreateUser();
 
-        //        await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-        //        await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-        //        var CreateUserResult = await _userManager.CreateAsync(user, Input.Password);
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var CreateUserResult = await _userManager.CreateAsync(user, Input.Password);
 
-        //        if (CreateUserResult.Succeeded)
-        //        {
-        //            _logger.LogInformation("User created a new account with password.");
+                if (CreateUserResult.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
 
-        //            //////
-        //            ///
+                    //////
+                    ///
 
-        //            /// Si j'ai bien compris on n'en a pas besoin car process d'enregistrement par email
+                    /// Si j'ai bien compris on n'en a pas besoin car process d'enregistrement par email
 
-        //            /*
-        //            var role = new IdentityRole {Name = user.Name};
-        //            var CreateRoleResult = await _roleManager.CreateAsync(role);
 
-        //            if (CreateRoleResult.Succeeded)
-        //            {
-        //                await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("FirstName", user.FirstName ));
-        //                await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("LastName", claim.Value));
+                    var role = new IdentityRole { Name = user.Name };
+                    var CreateRoleResult = await _roleManager.CreateAsync(role);
 
-        //                await _userManager.AddClaimAsync(identityUser, new System.Security.Claims.Claim(claim.Type, claim.Value));
-        //            }
+                    if (CreateRoleResult.Succeeded)
+                    {
+                        await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("FirstName", user.FirstName));
+                        await _roleManager.AddClaimAsync(role, new System.Security.Claims.Claim("LastName", claim.Value));
 
-        //        }
-        //                foreach (var claim in user.Claims)
-        //                {
-        //                }
+                        await _userManager.AddClaimAsync(identityUser, new System.Security.Claims.Claim(claim.Type, claim.Value));
+                    }
 
-        //                foreach (var role in user.Roles)
-        //                {
-        //                    await userManager.AddToRoleAsync(identityUser, role);
-        //                }
+                }
+                foreach (var claim in user.Claims)
+                {
+                }
 
-        //            */
+                foreach (var role in user.Roles)
+                {
+                    await userManager.AddToRoleAsync(identityUser, role);
+                }
 
-        //            //////
 
-        //            var userId = await _userManager.GetUserIdAsync(user);
-        //            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-        //            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-        //            var callbackUrl = Url.Page(
-        //                "/Account/ConfirmEmail",
-        //                pageHandler: null,
-        //                values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-        //                protocol: Request.Scheme);
 
-        //            await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-        //                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                //////
 
-        //            if (_userManager.Options.SignIn.RequireConfirmedAccount)
-        //            {
-        //                return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-        //            }
-        //            else
-        //            {
-        //                await _signInManager.SignInAsync(user, isPersistent: false);
-        //                return LocalRedirect(returnUrl);
-        //            }
-        //        }
-        //        foreach (var error in CreateUserResult.Errors)
-        //        {
-        //            ModelState.AddModelError(string.Empty, error.Description);
-        //        }
-        //    }
+                var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
 
-        //    // If we got this far, something failed, redisplay form
-        //    return Page();
-        //}
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in CreateUserResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
 
         private ApplicationUser CreateUser()
         {
